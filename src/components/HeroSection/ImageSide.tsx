8e00b54b3392c79521af9332968b19342d014f40
Ali-hassan23 <127784171+Ali-hassan23@users.noreply.github.com>
2024-06-28 15:14:59 +0500

    image added

--- src/components/HeroSection/ImageSide.tsx
+++ src/components/HeroSection/ImageSide.tsx
@@ -24,24 +24,30 @@ const ImageSide = () => {
   }, [controls]);
 
   return (
-    <div className="bg-gray-500 sm:h-screen h-1/2 sm:w-1/2 w-full"></div>
-    // <div className="h-screen w-1/2 overflow-hidden">
-    //   <motion.div
-    //     className="bg-black h-full"
-    //     initial={{ x: "100%" }}
-    //     animate={controls}
-    //   >
-    //     <div className="flex items-end justify-center w-full h-screen">
-    //       <motion.div
-    //         className="w-72 h-80 "
-    //         initial={{ y: "70%", opacity: 0 }}
-    //         animate={controls}
-    //       >
-    //         <Image src="/imgHa.png" width={200} height={500} alt="My Image" className="w-full "/>
-    //       </motion.div>
-    //     </div>
-    //   </motion.div>
-    // </div>
+    // <div className="bg-gray-500 sm:h-screen h-1/2 sm:w-1/2 w-full"></div>
+    <div className="h-screen w-1/2 overflow-hidden">
+      <motion.div
+        className="bg-black h-full"
+        initial={{ x: "100%" }}
+        animate={controls}
+      >
+        <div className="flex items-end justify-center w-full h-screen">
+          <motion.div
+            className="w-72 h-80 "
+            initial={{ y: "70%", opacity: 0 }}
+            animate={controls}
+          >
+            <Image
+              src="/imgHa.png"
+              width={200}
+              height={500}
+              alt="My Image"
+              className="w-full "
+            />
+          </motion.div>
+        </div>
+      </motion.div>
+    </div>
   );
 };
 
